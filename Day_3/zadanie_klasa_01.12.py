89de19776229ad79dcb794ecb5594f03604ac0ac
kdominikgostkowski <kdominikgostkowski@leroymerlin.pl>
2022-12-02 08:48:24 +0100

    klasy do zrobienia 02.12

--- Day_3/zadanie_klasa_01.12.py
+++ Day_3/zadanie_klasa_01.12.py
@@ -1,4 +1,5 @@
 from collections import defaultdict
+from fpdf import FPDF
 
 
 class World_cup:
@@ -39,3 +40,23 @@ print(book.average_grade('Polska'))
 book.add_team('Meksyk')
 book.report_grade('Meksyk','grupa 1', 5, 7, 3)
 print(book.average_grade('Meksyk'))
+
+document = FPDF()
+
+document.set_font('Times', "B", 14)
+document.set_text_color(19, 83, 173)
+document.add_page()
+document.cell(0, 5, 'Grupy na MS')
+document.ln()
+
+document.set_font('Times', '', 12)
+document.set_text_color(0)
+document.set_xy(10,40)
+document.multi_cell(w = 30, h = 5, txt="Grupa 1\nArgentyna\nPolska\nMeksk\nArabia", border=1)
+document.set_xy(40,40)
+document.multi_cell(w = 30, h = 5, txt="Grupa 2\nAnglia\nUSA\nIran\nWalia", border=1)
+document.set_xy(70,40)
+document.multi_cell(w = 30, h = 5, txt="Grupa 3\nFrancja\nAustralia\nTunezja\nDania", border=1)
+document.ln()
+
+document.output('grupaWC.pdf')
